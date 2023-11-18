2d155e8cf4a1f8eecceebd68bf5ca1fbb8e319e4
lakshaysehrawat <123374457+lakshaysehrawat@users.noreply.github.com>
2023-11-18 18:38:45 +0530

    Add files via upload

--- air-pollution.js
+++ air-pollution.js
@@ -41,42 +41,45 @@ const getAirQuality = async (lat, lon) => {
 }
 
 const setValuesOfAir = airData => {
-	const aqi = airData.list[0].main.aqi
-	let airStat = "", color = ""
+	const aqi = airData.list[0].main.aqi;
+	let airStat = "", color = "";
 
 	// Set Air Quality Index
-	airQuality.innerText = aqi
+	airQuality.innerText = aqi;
+
+	// Display real AQI value
+	document.querySelector('.aqi-value').innerText = aqi;
 
 	// Set status of air quality
-
 	switch (aqi) {
 		case 1:
-			airStat = "Good"
-			color = "rgb(19, 201, 28)"
-			break
-			case 2:
-				airStat = "Fair"
-				color = "rgb(15, 134, 25)"
-				break
-			case 3:
-				airStat = "Moderate"
-				color = "rgb(201, 204, 13)"
-				break
-			case 4:
-				airStat = "Poor"
-				color = "rgb(204, 83, 13)"
-				break
+			airStat = "Good";
+			color = "rgb(19, 201, 28)";
+			break;
+		case 2:
+			airStat = "Fair";
+			color = "rgb(15, 134, 25)";
+			break;
+		case 3:
+			airStat = "Moderate";
+			color = "rgb(201, 204, 13)";
+			break;
+		case 4:
+			airStat = "Poor";
+			color = "rgb(204, 83, 13)";
+			break;
 		case 5:
-			airStat = "Very Poor"
-			color = "rgb(204, 13, 13)"
-			break
+			airStat = "Very Poor";
+			color = "rgb(204, 13, 13)";
+			break;
 		default:
-			airStat = "Unknown"
+			airStat = "Unknown";
 	}
 
-	airQualityStat.innerText = airStat
-	airQualityStat.style.color = color
+	airQualityStat.innerText = airStat;
+	airQualityStat.style.color = color;
 }
+
 
 const setComponentsOfAir = airData => {
 	let components = {...airData.list[0].components}
@@ -107,6 +110,12 @@ srchBtn.addEventListener("click", () => {
 //     .catch(() => {
 //         result.textContent = 'Error fetching data. Please try again later.';
 //     });
+// Inside the setValuesOfAir function, after setting airQualityStat and color
+// const realAQIValue = airData.list[0].main.aqi;
+// document.querySelector('.aqi-value').innerText = realAQIValue;
+
+// Inside the getAirQuality function, after setting components of air
+
 
 
 getUserLocation()
